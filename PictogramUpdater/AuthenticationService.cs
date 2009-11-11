fa5b1114474fc5fc0a87b74d0f78b09b009ae2fe
knifhen <knifhster@gmail.com>
2009-11-11 21:13:22 +0100

    Replaced hacked read of .ini with AMS.Profile

---
+++ PictogramUpdater/AuthenticationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using AMS.Profile;
+
+namespace PictogramUpdater {
+    internal class AuthenticationService {
+        private ISettingsPersistence settings;
+
+        public AuthenticationService(ISettingsPersistence settings) {
+            this.settings = settings;
+        }
+
+        public Boolean IsPictogramManagerInstalled() {
+            var iniFilePath = Environment.GetEnvironmentVariable("WINDIR") + "\\PictogramManager.ini";
+            var iniFile = new FileInfo(iniFilePath);
+            if (!iniFile.Exists) {
+                return false;
+            }
+            var ini = new Ini(iniFilePath);
+            var exeFilePath = ini.GetValue("Inst", "Dir") as string;
+            if(exeFilePath == null) {
+                return false;
+            }
+            var exeFile = new FileInfo(exeFilePath + "\\PictogramManager.exe");
+            return exeFile.Exists;
+        }
+
+        public String GetUsername() {
+            return IsPictogramManagerInstalled() ? "webservice" : settings.getProperty("username");
+        }
+
+        public String GetPassword() {
+            return IsPictogramManagerInstalled() ? "tbn2wswzcrf4" : settings.getProperty("password");
+        }
+    }
+}
