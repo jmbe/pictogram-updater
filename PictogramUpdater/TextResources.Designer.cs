4a06f134c269d978b4c3ed35ee15fab60b00d5cb
JM Bergqvist <jm.bergqvist@gmail.com>
2021-08-05 00:42:56 +0200

    Bump version

--- PictogramUpdater/TextResources.Designer.cs
+++ PictogramUpdater/TextResources.Designer.cs
@@ -133,7 +133,7 @@ namespace PictogramUpdater {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 3.14.0.0.
+        ///   Looks up a localized string similar to 3.14.1.0.
         /// </summary>
         internal static string currentVersion {
             get {
